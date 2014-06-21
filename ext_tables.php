dfeb258e8e4f4587d28d7400638560155cdc501e
Kay Strobach <info@kay-strobach.de>
2014-06-21 13:29:13 +0200

    [TASK] Cleanup

--- ext_tables.php
+++ ext_tables.php
@@ -2,7 +2,7 @@
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/template.php']['preHeaderRenderHook'][] = 'EXT:dyncss_test/Classes/Be/PreHeaderRenderHook.php:Tx_DyncssTest_Be_PreHeaderRenderHook->main';
 
-t3lib_extMgm::addStaticFile(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
 	'dyncss_test',
 	'Resources/Private/TypoScript',
 	'dyncss_test'
